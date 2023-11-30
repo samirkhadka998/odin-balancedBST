496818f01341dfed0519f4cfbf5b283480c61fe1
Samir Khadka <47472827+samirkhadka998@users.noreply.github.com>
2023-11-30 07:32:19 +0545

    Convert to balance tree function implemented

--- script.js
+++ script.js
@@ -12,7 +12,9 @@ class Tree{
     }
 }
 
-let arr =  [1, 7, 4];
+// let arr =   [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
+let arr = [1,2,3,4,5]
+let tree = new Tree(null);
 buildTree(arr)
 function buildTree(arr){
     //remove duplicate
@@ -21,7 +23,21 @@ function buildTree(arr){
     //sort
     arr = (mergeSort(arr));
     //return 0 level node
-    console.log(arr)
+    tree.root = convertToBalanceTree(arr,0, arr.length - 1);
+    console.log(tree.node);
+    // prettyPrint(tree.root)
+}
+
+function convertToBalanceTree(arr, start, end){
+    if(start > end){
+        return null;
+    }
+
+    let mid = parseInt((start + end) / 2);
+    let node = new Node(arr[mid])
+    node.left = convertToBalanceTree(arr, start, mid - 1);
+    node.right = convertToBalanceTree(arr, mid + 1 , end);
+    return node;
 }
 
 function mergeSort(arr) {
@@ -54,3 +70,18 @@ function merge(left, right) {
 
     return [...newArr, ...left, ...right];
 }
+
+
+
+function prettyPrint(node, prefix = "", isLeft = true) {
+    if (node === null) {
+      return;
+    }
+    if (node.right !== null) {
+      prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+    }
+    console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+    if (node.left !== null) {
+      prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+    }
+  };
